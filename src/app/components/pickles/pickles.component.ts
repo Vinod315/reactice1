13e22b079fc067e5f8ad1ac3890cda3368d95969
vinod <vinod@gmail.com>
2025-07-24 18:02:00 +0530

    I have added for loop for all pickles

--- src/app/components/pickles/pickles.component.ts
+++ src/app/components/pickles/pickles.component.ts
@@ -6,6 +6,16 @@ import { CommonModule } from '@angular/common';
 interface PickleItem {
   pickleName: string;
   Price: number;
+ 
+}
+
+interface Pickles {
+  id: number;
+  name: string;
+  content: string;
+  image: string;
+  type:string;
+  price: number;
 }
 
 @Component({
@@ -18,21 +28,40 @@ interface PickleItem {
 export class PicklesComponent {
 
   pickleType: any = undefined;
-   item: PickleItem = {
-   pickleName: '',
-  Price:0
+  item: PickleItem = {
+    pickleName: '',
+    Price: 0,
+  
   };
   value = 0;
   num = 0;
   showAddedAlert = false;
+  pickles :Pickles[]=[];
 
   constructor(private cartService: CartService) {
 
-  
+
   }
+  ngOnInit(){
+  this.pickles=[
+    {id:1, name:"Mango Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"Mango.webp",type:"veg", price:600},
+    {id:2, name:"Allam Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"Allam.webp",type:"veg", price:500},
+    {id:3, name:"Amla Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"Amla.webp", type:"veg",price:600},
+    {id:4, name:"Leamon Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"Lemon.webp",type:"veg", price:500},
+    {id:5, name:"Tomato Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"Tomato.webp",type:"veg", price:600},
+    {id:6, name:"Red-Chilli Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"Gongura.webp",type:"veg", price:400},
+    {id:7, name:"Chicken Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"non-veg/Chicken.webp", type:"non-veg",price:1000},
+    {id:8, name:"Prawns Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"non-veg/Prawns.webp",type:"non-veg", price:1800},
+    {id:9, name:"Chicken Boneless Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"non-veg/Chicken.webp",type:"non-veg", price:1400},
+    {id:10, name:"Mutton Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"non-veg/Mutton.webp",type:"non-veg", price:1800},
+    {id:11, name:"Mutton Boneless Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"non-veg/Mutton.webp", type:"non-veg",price:2000},
+    {id:12, name:"Mutton-keema Pickle", content:"These pickles may cause sudden snack attacks and unsolicited “mmm” sounds",image:"non-veg/MuttonCheema.webp",type:"non-veg", price:2200},
+
+  ];}
+
   cartButtonClicked() {
     this.value = this.value + 1;
-    console.log("Mango button clicked ")
+    console.log("Mango button clicked ");
   }
 
   cartButton(PickleName: string, Price: number) {
@@ -42,17 +71,16 @@ export class PicklesComponent {
     setTimeout(() => {
       this.showAddedAlert = false;
     }, 1500);
-    
-    this.item = { pickleName: PickleName, Price: Price };
-    this.cartService.updateSelectedItem(this.item);
-    
-   
 
-  }
+    this.item = { pickleName: PickleName, Price: Price};
+    this.cartService.updateSelectedItem(this.item);
+  };
+
   veg() {
     this.pickleType = "Veg";
     console.log("veg clicked")
   }
+  
   nonveg() {
     this.pickleType = "NonVeg";
     console.log("non-veg clicked")
--- src/app/components/view-cart/view-cart.component.ts
+++ src/app/components/view-cart/view-cart.component.ts
@@ -1,23 +1,39 @@
 import { Component ,input, OnInit} from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-view-cart',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './view-cart.component.html',
   styleUrl: './view-cart.component.css'
 })
 export class ViewCartComponent {
 DetailsArray :any[]=[];
 constructor(private cartService:CartService){}
-  
+  id=0
+
   ngOnInit() {
     this.cartService.itemDetailsArray$.subscribe(items => {
       this.DetailsArray = items;
+      for(let i=0; i< this.DetailsArray.length;i++){
+        const item=this.DetailsArray[i];
+           item.quantity=1;
+           item.id=i;
+       
+      }
+      this.DetailsArray.push
       console.log('Items in cart:', this.DetailsArray);
     });
   }
+  quantityIncrease(id:number){
+   this.DetailsArray[id].quantity+=1;
+   console.log(id);
+  }
+  // quantityDecrease(){
+    
+  }
 
-}
+
